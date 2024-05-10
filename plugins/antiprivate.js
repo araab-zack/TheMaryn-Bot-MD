fc8c2d67ab35b7eeb5378dd81e6a437a3b329c6c
Elmoz3gg <162893646+Elmoz3gg@users.noreply.github.com>
2024-05-11 02:28:25 +0300

    Update antiprivate.js

--- plugins/antiprivate.js
+++ plugins/antiprivate.js
@@ -6,7 +6,7 @@ export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) 
   const chat = global.db.data.chats[m.chat];
   const bot = global.db.data.settings[this.user.jid] || {};
   if (bot.antiPrivate && !isOwner && !isROwner) {
-    await m.reply(`*[❗] ʜᴏʟᴀ @${m.sender.split`@`[0]}, كلم المطور لرفع لحظر عنكك او ادخل جروب دعم رسمي للبوت المطور: +201159106301 يمنع التحدث بالرسائل الخاصة للبوت وبالتالي سيتم حظرك.*`, false, {mentions: [m.sender]});
+    await m.reply(`*[❗] مرحبا @${m.sender.split`@`[0]}, سيتم حظرك من البوت ممنوع الدخول للبوت خاص ياورع روح كلم مطوري يضيفوه لك في نقابتك +201280244814 يلا باي هابي بلوك*`, false, {mentions: [m.sender]});
     await this.updateBlockStatus(m.chat, 'block');
   }
   return !1;
