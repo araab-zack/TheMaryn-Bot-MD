4ec75a0a1e9e68831780319acfed249210a12c43
Elmoz3gg <162893646+Elmoz3gg@users.noreply.github.com>
2024-05-11 15:49:38 +0300

    Update كت2.js

--- plugins/كت2.js
+++ plugins/كت2.js
@@ -19,7 +19,7 @@ let handler = async (m, { conn, usedPrefix }) => {
 
 ❐↞┇الـوقـت⏳↞ ${(timeout / 1000).toFixed(2)}┇
 ❐↞┇الـجـائـزة💰↞ ${poin} نقاط┇
-『💧𝐕𝐄𝐑𝐋𝐃┊🤖┊𝐁𝐎𝐓💧』
+『🍷𝐌𝐀𝐑𝐘𝐍┊🤖┊𝐁𝐎𝐓🍷』
 `.trim();
     conn.tekateki[id] = [
        await conn.reply(m.chat, caption, m),
